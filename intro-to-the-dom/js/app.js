d31e758bc97da2d2af644a27089d39e6e6ff0941
dbryant <d.bryant718@gmail.com>
2024-10-25 13:01:59 -0400

    started flexbox

---
+++ intro-to-the-dom/js/app.js
@@ -0,0 +1,27 @@
+
+const bodyElement = document.querySelector('body');
+const titleElement = document.querySelector('#main-title')
+const paragraphElement = document.querySelector('.lyric');
+
+// modify elements
+paragraphElement.textContent = 'New Paragraph'
+titleElement.style.textAlign = 'center';
+titleElement.style.color = 'orange';
+commentElements = document.querySelectorAll('#comments')
+
+const h2Element = document.createElement('h2')
+h2Element.textContent = "comment section"
+
+console.log(paragraphElement.textContent);
+
+modyElement.appendChild
+
+// console directory
+console.dir(bodyElement)
+console.dir(titleElement);
+console.dir(paragraphElement)
+
+commentElements.forEach((commentElements))=>(
+    console.log(commentEl.);
+    comentEl.style.fontSize = '30px';
+) 
